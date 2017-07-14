d036ad479349e0f7cfb5db64e90b1186206f4dff
SemanticBeeng <nick@semanticbeeng.com>
2017-07-14 09:24:11 -0700

    marking code from functional and archicture perspective
    introducing `mypy` typing
    
    partial changes: does not run to the end due to
    
    ```
    DEBUG:root:Measuring ranks...
    Traceback (most recent call last):
      File "/development/projects/04_clients/czi/ds/The_Termolator/distributional_component.py", line 103, in <module>
        __main__(sys.argv)
      File "/development/projects/04_clients/czi/ds/The_Termolator/distributional_component.py", line 88, in __main__
        ranking = metric.rankTerms(measure)
      File "/development/projects/04_clients/czi/ds/The_Termolator/Metric.py", line 359, in rankTerms
        temp = self.metrics[measure](w)
      File "/development/projects/04_clients/czi/ds/The_Termolator/Metric.py", line 335, in _calWeighted
        ret += self.weights[measure] * self.metrics[measure](word)
      File "/development/projects/04_clients/czi/ds/The_Termolator/Metric.py", line 202, in _calTokenIDF
        return self._calIDF(word) * self._calTokenDR(word)
      File "/development/projects/04_clients/czi/ds/The_Termolator/Metric.py", line 189, in _calTokenDR
        tokenDR += token_rel / float(token_total)
    ZeroDivisionError: float division by zero
    calling filter_term_output.py with filter_term_output.py DAVETEST DAVETEST.outputweb.score False 8400
    Traceback (most recent call last):
      File "/development/projects/04_clients/czi/ds/The_Termolator/filter_term_output.py", line 31, in <module>
        if __name__ == '__main__': sys.exit(main(sys.argv))
      File "/development/projects/04_clients/czi/ds/The_Termolator/filter_term_output.py", line 29, in main
        filter_terms(input_file,output_file,abbr_full_file,full_abbr_file,use_web_score,numeric_cutoff=max_term_number,reject_file=reject_file,web_score_dict_file=web_score_dict_file)
      File "/development/projects/04_clients/czi/ds/The_Termolator/filter_terms.py", line 959, in filter_terms
        confidence_cutoff = output[confidence_position][0]
    IndexError: list index out of range
    Final terms can be found in DAVETEST.out_term_list from the scored file in DAVETEST.scored_output
    head: cannot open 'DAVETEST.scored_output' for reading: No such file or directory
    ```
    working!
    have to call
    ```
     $TERMOLATOR/run_termolator.sh $NYU_DIR/foreground.list $NYU_DIR/background.list .txt DAVETEST "True" "False" 8400 1400 $NYU_DIR "" "" "False"
     ```
    
    mypy inline_terms.py - no errors
    
    mypy DataDef.py  now does not give errors
    
    as desired, it does type checking
    `DataDef.py:33: error: Incompatible types in assignment (expression has type FileName[TERMS], variable has type FileName[POS])`

--- DataDef.py
+++ DataDef.py
@@ -4,9 +4,19 @@ import sys
 # @resource https://www.python.org/dev/peps/pep-0484/#user-defined-generic-types
 
 DT = TypeVar('DT')
-POS = str
-TERMS = int
-ABBR = str
+
+class TP:
+    def __init__(self, v: str) -> None:
+        self.v = v
+
+class POS(TP):
+    pass
+
+class TERMS(TP):
+    pass
+
+class ABBR(TP):
+    pass
 
 class FileName(Generic[DT]):
     def __init__(self, name: DT) -> None:
@@ -17,8 +27,8 @@ class FileName(Generic[DT]):
 
 
 def main(args: List[str]):
-    posFile1 = FileName[POS]("123.pos")
-    termsFile2 = FileName[TERMS]("123.terms")
+    posFile1 = FileName(POS("123.pos"))
+    termsFile2 = FileName(TERMS("123.terms"))
 
     posFile1 = termsFile2
 
