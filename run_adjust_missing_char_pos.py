110423c4117c78d4d2197c90756e91412d20fcfd
SemanticBeeng <nick@semanticbeeng.com>
2017-07-19 03:03:39 -0700

    (wip) Applying the migration approach (aka "in-place", "meet in the middle")
    1. use `typing` and `mypy` to introduce types for clarification and to remove use of dynamic typing
    2. refactor global variables: encapsulate, identify read-only vs mutable
    3. refactor to clarify types of data elements and encapsulate them in entities
    4. refactor to encapsulate IO towards being able to replace with scalable data management
    5. replace Python specfic binary formats like pickle with interchangeable ones (json)
    https://stackoverflow.com/questions/4547274/convert-a-python-dict-to-a-string-and-back
    
    regression testing
    
    ```
    $TERMOLATOR/run_termolator.sh $NYU_DIR/foreground.list $NYU_DIR/background.list .txt DAVETEST "True" "False" 8400 1400 $TERMOLATOR  "" "" "False"
    sort master/DAVETEST.all_terms > master/DAVETEST.all_terms.sorted
    sort improvements/DAVETEST.all_terms > improvements/DAVETEST.all_terms.sorted
    
    sort master/DAVETEST.scored_output > master/DAVETEST.scored_output.sorted
    sort improvements/DAVETEST.scored_output > improvements/DAVETEST.scored_output.sorted
    
    # not diff
    diff /development/projects/04_clients/czi/ds/The_Termolator/tmpexec/master/DAVETEST.all_terms.sorted /development/projects/04_clients/czi/ds/The_Termolator/tmpexec/improvements/DAVETEST.all_terms.sorted
    
    # some diff at the 4th decimal
    bcompare /development/projects/04_clients/czi/ds/The_Termolator/tmpexec/master/DAVETEST.scored_output.sorted /development/projects/04_clients/czi/ds/The_Termolator/tmpexec/improvements/DAVETEST.scored_output.sorted
    ```
    
    removed unused file open
    more clarification of entities
    
    removed unused reference to  "*.dict_full_to_abbr" (data flow)
    introduce more types for File
     rename FileName -> File
    
     @func refactoring
    
     code review, formatting
    
     removed dead code
    
     data flow cleanup:
     marking @arch global state mutations and references
    
     code review, formatting, typing
    
     marking @arch global state mutations and references
    
     code review, formatting, typing
    
      removed dead code: @todo check with Adam about unused dictionaries
    
     code review, formatting, typing
    
    @func refactoring : moved all file based dictionary mgmt code to dictionary.py to
     - encapsulate vars and code
     - scope the global variables
     - make access to them more explicit (visible)
    
     using `frozendict` to verify runtime immutability of readonly dictionaries
     https://stackoverflow.com/a/41368892/4032515
    
    code review, typing, light refactoring to achieve static typing
    
    typing: adding more uses of File
     using explicit parameters for calls to File.openText
    
    more uses of File : not critical
    removed duplicate dir_name and using Settings instead
    
    typing: adding more uses of File
    
    static typing: sensitive change to use Tuple instead of [] and None instead of True/False to represent absence of a value
    
    (status) works

--- run_adjust_missing_char_pos.py
+++ run_adjust_missing_char_pos.py
@@ -3,20 +3,28 @@
 import os
 import re
 import sys
+from typing import List, Tuple, Optional, Pattern, Match
+from DataDef import File, ABBR, POS
 
+#
+#
+#
+def get_pos_facts(fact_list: List[ABBR]) -> List[Tuple[int, int, str]]:
+    output: List[Tuple[int, int, str]] = []
+    bad_pos_pattern: Pattern[str] = re.compile('BAD_CHARACTER START=([0-9]*) END=([0-9]*) STRING="([^"]*)"')
 
-def get_pos_facts(fact_list):
-    output = []
-    bad_pos_pattern = re.compile('BAD_CHARACTER START=([0-9]*) END=([0-9]*) STRING="([^"]*)"')
     for fact in fact_list:
-        bad_match = bad_pos_pattern.search(fact)
+        bad_match: Optional[Match[str]] = bad_pos_pattern.search(fact)
         if bad_match:
-            output.append([int(bad_match.group(1)), int(bad_match.group(2)), bad_match.group(3)])
+            output.append((int(bad_match.group(1)), int(bad_match.group(2)), bad_match.group(3)))   # @semanticbeeng @todo static typing
     return (output)
 
 
-def make_fact_pair(triple):
-    output = [triple[0], triple[2] + ' ||| S:' + str(triple[0]) + ' E:' + str(triple[1]) + ' ||| ' + 'SYM' + os.linesep]
+#
+#
+#
+def make_fact_pair(triple: Tuple[int, int, str]) -> Tuple[int, str]:
+    output = (triple[0], triple[2] + ' ||| S:' + str(triple[0]) + ' E:' + str(triple[1]) + ' ||| ' + 'SYM' + os.linesep)  # @semanticbeeng @todo static typing
     return (output)
 
 
@@ -40,17 +48,24 @@ def modify_pos_end(line, new_end):
         return (line)
 
 
-def fix_bad_char_in_file(fact, pos):
-    with open(fact) as fact_stream:
-        fact_list = get_pos_facts(fact_stream.readlines())
+#
+#
+#
+def fix_bad_char_in_file(fact: File[ABBR], pos: File[POS]):
+
+    with fact.openText() as fact_stream:
+        fact_list: List[Tuple[int, int, str]] = get_pos_facts(fact_stream.readlines())
         fact_list.reverse()
+
     if fact_list:
-        with open(pos) as pos_stream:
+        with pos.openText() as pos_stream:
             pos_list = pos_stream.readlines()
             pos_list.reverse()
-        next_fact = False
-        next_pos = False
-        with open(pos, 'w') as outstream:
+
+        next_fact = None        # @semanticbeeng @todo static typing
+        next_pos = None         # @semanticbeeng @todo static typing
+
+        with pos.openText(mode='w') as outstream:
             while pos_list or fact_list:
                 if fact_list and (not next_fact):
                     next_fact = make_fact_pair(fact_list.pop())
@@ -58,22 +73,22 @@ def fix_bad_char_in_file(fact, pos):
                     next_pos = make_pos_triple(pos_list.pop())
                 if (next_pos and next_fact and (next_pos[0] > next_fact[0])) or (next_fact and not next_pos):
                     outstream.write(next_fact[1])
-                    next_fact = False
+                    next_fact = None       # @semanticbeeng @todo static typing
                 elif next_pos:
                     if next_fact and (next_fact[0] > next_pos[0]) and (next_fact[0] < next_pos[1]):
                         outstream.write(modify_pos_end(next_pos[2], next_fact[0]))
                     else:
                         outstream.write(next_pos[2])
-                    next_pos = False
+                    next_pos = None        # @semanticbeeng @todo static typing
 
 
 def main(args):
     file_list = args[1]
     lines = []
-    with open(file_list) as instream:
-        for line in instream:
+    with File(file_list).openText() as instream:
+        for line in instream.readlines():
             fact, pos = line.strip().split(';')
-            fix_bad_char_in_file(fact, pos)
+            fix_bad_char_in_file(File[ABBR](fact), File[POS](pos))
 
 
 if __name__ == '__main__': sys.exit(main(sys.argv))
