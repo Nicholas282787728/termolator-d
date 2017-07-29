b6a21f75d173365462b3b91f4aadb8f3a4d41744
SemanticBeeng <nick@semanticbeeng.com>
2017-07-29 08:04:21 -0700

    (checkpoint) regression tested with all data set and the results are consistemt with past tests Only small differences to *.terms (known issue) and "same" all.terms and scores.
    Used NYU_DIR=/development/projects/04_clients/czi/ds/The_Termolator/tests/czi/DAVETEST_all/source

--- termolatorj/src/main/scala/org/czi/termolator/testing/find_inline_terms_tests.scala
+++ termolatorj/src/main/scala/org/czi/termolator/testing/find_inline_terms_tests.scala
@@ -19,6 +19,17 @@ object find_inline_terms_tests extends App {
   val file = NYU_DIR + "foreground/19729111"
 
   /**
+    * @todo more files to terst: difference in [[org.czi.termolator.porting.DataDef.TERMS]] files
+    */
+  private val DAVETEST_all_root = "/development/projects/04_clients/czi/ds/The_Termolator/tests/czi/DAVETEST_all/source/"
+  DAVETEST_all_root + "foreground/26718012.terms"
+  DAVETEST_all_root + "foreground/26717674.terms"
+  DAVETEST_all_root + "foreground/26717671.terms"
+  DAVETEST_all_root + "foreground/26717670.terms"
+  DAVETEST_all_root + "foreground/26717663.terms"
+  DAVETEST_all_root + "foreground/19669636.terms"
+
+  /**
     * @todo cannot assert anything about this becuase it is monolithical
     */
   find_terms.find_inline_terms_for(file, start = true)
